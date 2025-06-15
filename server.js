a94ba2aadb799d3f1913879ec0deca8203773e62
Nuya52 <alexrebrikov9@gmail.com>
2025-06-15 11:47:56 +0300

    udjvjddvj

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const http = require('http');
-
-
-
 const fs = require('fs');
 const path = require('path');
 
 const pathToIndex = path.join(__dirname, 'static', 'index.html');
+const pathToStyle = path.join(__dirname, 'static', 'style.css');
+const pathToScript = path.join(__dirname, 'static', 'index.js');
 const indexHtmlFile = fs.readFileSync(pathToIndex);
 
 const server = http.createServer((req, res) => {
-    if(req.url == '/'){
-        return res.end(indexHtmlFile)
+    // if(req.url == '/'){
+    //     return res.end(indexHtmlFile);
+    // }
+    switch(req.url){
+        case '/': return res.end(indexHtmlFile);
+        case '/style.css': return res.end(pathToStyle);
+        case '/index.js': return res.end(pathToScript);
     }
     req.statusCode = 404;
     return res.end('Error 404');
 });
+
+
 server.listen(3000);
+const {Server} = require('socket.io');
+const io = new Server(server);
+io.join('connection', (socket) =>{
+    console.log('a user connected. id - ' + socket.io);
+});
--- static/index.js
+++ static/index.js
@@ -1,1 +1,2 @@
 alert('Welcome to chat only for devchat')
+const socket = io();
